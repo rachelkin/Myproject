5e9c0bd9c8353f17a8f87912507a8c99423c0f21
קינסבורסקי רחל <1637351@main.local>
2025-11-02 13:28:50 +0200

    copy

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,25 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { User } from './user/user';
+import { USERS } from './fake_users';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,User],
+  imports: [RouterOutlet , Header,User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
+
 export class App {
-  protected readonly title = signal('first');
+
+  protected readonly title = signal('my-first-app');
+  users = USERS;
 }
+
+
+
+
+
+
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { USERS } from '../fake_users';
+import { Component, Input } from '@angular/core';
+import { USERS} from '../fake_users';
+
 
 @Component({
   selector: 'app-user',
@@ -7,16 +8,17 @@ import { USERS } from '../fake_users';
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
+
 export class User {
-  selectedUser=USERS[0];
-   changeUser=()=>{
-   const randomIndex = Math.floor(Math.random() * USERS.length);
-   this.selectedUser=USERS[randomIndex];
+  selectedUser= USERS[0];
+  @Input() name!: string;
+  @Input() avatar!: string;
+  
+  get  userImgPath(){
+    return 'assets/users/' + this.avatar;
   }
-
-  get userImgPath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-    }
-    
-
-    }
+  changeUser(){
+    const randomIndex = Math.floor(Math.random() * USERS.length);
+    this.selectedUser =USERS[randomIndex];
+  }
+}
