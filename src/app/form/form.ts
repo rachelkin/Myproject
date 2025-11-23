a45b301de068425e3bf4b68e3286e76349ae7ecc
rachelkin <rachel67960@gmail.com>
2025-11-23 17:14:07 +0200

    add form

---
+++ src/app/form/form.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms'
+import { FormGroup,FormControl ,Validators  } from '../../../node_modules/@angular/forms/index';
+
+@Component({
+  selector: 'app-form',
+  imports: [],
+  templateUrl: './form.html',
+  styleUrl: './form.css'
+})
+export class Form {
+form=new FormGroup({
+  firstName: new FormControl('',[Validators.required,Validators.maxLength(18)]),
+  lastName: new FormControl('',[Validators.required,Validators.maxLength(18)]),
+  adress: new FormControl('',[Validators.required]),
+  email: new FormControl('',[Validators.required, Validators.email]),
+  passwoed1: new FormControl('',[Validators.required, Validators.minLength(6)]),
+  passwoed2: new FormControl('',[Validators.required, Validators.minLength(6)]),
+  category: new FormControl('', [Validators.required]) 
+
+});
+}
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './header.css'
 })
 export class Header {
-
+  showForm = false;
 }
 
 
